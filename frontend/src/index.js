cadbfa9ab24538fa7e309d2a61d7ab36448ad29d
Younes Alturkey <me@younes.expert>
2024-05-31 18:39:43 +0300

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom"
 import App from "./App"
 import "./index.css"
 
-axios.defaults.baseURL = "https://bitchunk.younes.ai"
+axios.defaults.baseURL = "https://bitchunk.younes.expert"
 axios.defaults.headers.post["Content-Type"] = "application/json"
 
 ReactDOM.render(<App />, document.getElementById("root"))
